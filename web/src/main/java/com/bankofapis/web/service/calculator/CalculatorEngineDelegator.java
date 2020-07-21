a02b1bc6326065c963590fe3cfd55ad00545501c
Sumit945500 <sumit945500@gmail.com>
2020-07-21 15:36:33 +0530

    calculation delegator

---
+++ web/src/main/java/com/bankofapis/web/service/calculator/CalculatorEngineDelegator.java
@@ -0,0 +1,42 @@
+package com.bankofapis.web.service.calculator;
+
+import com.bankofapis.core.model.accounts.OBReadAccountInformation;
+import com.bankofapis.core.model.accounts.OBReadProduct;
+import com.bankofapis.core.model.accounts.OBReadTransaction;
+import com.bankofapis.web.service.CustomerDataService;
+import com.bankofapis.web.service.recommendation.ProductType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CalculatorEngineDelegator {
+
+    @Autowired
+    private CustomerDataService customerDataService;
+    @Autowired
+    private ProductSelectionCalculatorFactory productSelectionCalculatorFactory;
+    Map<String , OBReadProduct> productsToOffer = new LinkedHashMap<>();
+
+    public void executeFlow(){
+        Map<OBReadAccountInformation, List<OBReadTransaction>> obReadAccountInformationListMap
+                =customerDataService.getObReadAccountInformationListTransactionMap();
+        obReadAccountInformationListMap.forEach((obReadAccountInformation, obReadTransactions) -> {
+            HealthProductCalculator healthProductCalculator = (HealthProductCalculator) productSelectionCalculatorFactory.getProductSelectionCalculator(ProductType.HEALTH.getProductName());
+            LoanProductCalculator loanProductCalculator =(LoanProductCalculator)productSelectionCalculatorFactory.getProductSelectionCalculator(ProductType.LOAN.getProductName());
+            InvestmentProductCalculator investmentProductCalculator= (InvestmentProductCalculator) productSelectionCalculatorFactory.getProductSelectionCalculator(ProductType.INVESTMENT.getProductName());
+            healthProductCalculator.setAccountId(obReadAccountInformation.getAccountId());
+            loanProductCalculator.setAccountId(obReadAccountInformation.getAccountId());
+            investmentProductCalculator.setAccountId(obReadAccountInformation.getAccountId());
+            //execute only for saving bank product further can be checked for current bank & credit card
+            productsToOffer.putAll(healthProductCalculator.execute());
+            productsToOffer.putAll(loanProductCalculator.execute());
+            productsToOffer.putAll(investmentProductCalculator.execute());
+        });
+    }
+
+    public Map<String, OBReadProduct> getProductsToOffer() {
+        return productsToOffer;
+    }
+}
