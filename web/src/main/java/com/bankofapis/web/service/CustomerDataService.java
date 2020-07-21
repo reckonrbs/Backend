9688b932822f076a8e9a538b30d94519b8fd2cdc
Sumit945500 <sumit945500@gmail.com>
2020-07-21 14:21:59 +0530

    customerservice full impl

--- web/src/main/java/com/bankofapis/web/service/CustomerDataService.java
+++ web/src/main/java/com/bankofapis/web/service/CustomerDataService.java
@@ -23,4 +23,40 @@ public class CustomerDataService {
     public Map<OBReadAccountInformation, List<OBReadTransaction>> getObReadAccountInformationListTransactionMap() {
         return obReadAccountInformationListTransactionMap;
     }
+    //get Accounts List
+    public void fetchAccTxnInfo(){
+        accountResponse = aispService.getAccountResponse();
+        OBReadAccountList obReadAccountList  = accountResponse.getData();
+        List<OBReadAccountInformation> obReadAccountListAccount = obReadAccountList.getAccount();
+        obReadAccountListAccount.forEach(obReadAccountInformation -> {
+            OBReadDataResponse<OBReadTransactionList> obReadTransactionListOBReadDataResponse  = aispService.getTransactionsById(obReadAccountInformation.getAccountId());
+            OBReadTransactionList obReadTransactionList= obReadTransactionListOBReadDataResponse.getData();
+            obReadAccountInformationListTransactionMap.put(obReadAccountInformation,obReadTransactionList.getTransactionList());
+        });
+    }
+
+    public void fetchAccProductInfo(){
+        if(null != accountResponse){
+            OBReadAccountList obReadAccountList  = accountResponse.getData();
+        }else{
+            accountResponse  = aispService.getAccountResponse();
+        }
+        OBReadAccountList obReadAccountList  = accountResponse.getData();
+        List<OBReadAccountInformation> obReadAccountListAccount = obReadAccountList.getAccount();
+        obReadAccountListAccount.forEach(obReadAccountInformation -> {
+            OBReadDataResponse<OBReadProductList> obReadProductListOBReadDataResponse  = aispService.getProductById(obReadAccountInformation.getAccountId());
+            OBReadProductList obReadProductList = obReadProductListOBReadDataResponse.getData();
+            obReadAccountInformationListProductMap.put(obReadAccountInformation,obReadProductList.getProductList());
+        });
+    }
+    public List<OBReadProduct> fetchAccProductInfo(String accountId){
+        OBReadDataResponse<OBReadProductList> obReadProductListOBReadDataResponse = aispService.getProductById(accountId);
+        OBReadProductList obReadProductList=obReadProductListOBReadDataResponse.getData();
+        return obReadProductList.getProductList();
+    }
+    public List<OBReadTransaction> fetchAccTxnInfo(String accountId){
+        OBReadDataResponse<OBReadTransactionList> obReadTransactionListOBReadDataResponse= aispService.getTransactionsById(accountId);
+        OBReadTransactionList obReadTransactionList = obReadTransactionListOBReadDataResponse.getData();
+        return obReadTransactionList.getTransactionList();
+    }
 }
